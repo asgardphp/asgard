31a8c62b58a87c9d44281333927fe89c02396700
Michel Hognerud <leyou.m@gmail.com>
2014-04-22 12:27:46 +0800

    fixes

--- Bundle.php
+++ Bundle.php
@@ -3,7 +3,7 @@ namespace Asgard\Hook;
 
 class Bundle extends \Asgard\Core\BundleLoader {
 	public function load($queue) {
-		if(\Asgard\Core\App::get('autoloader'))
+		if(\Asgard\Core\App::has('autoloader'))
 			\Asgard\Core\App::get('autoloader')->preloadDir(__dir__);
 		
 		parent::load($queue);
