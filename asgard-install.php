0d0b23e8691c71183ba037f01e23824c11a2229e
Michel Hognerud <leyou.m@gmail.com>
2014-04-24 16:22:52 +0800

    fixes

--- asgard-install.php
+++ asgard-install.php
@@ -1,16 +1,28 @@
 <?php
 require_once 'paths.php';
-require_once _VENDOR_DIR_.'autoload.php'; #composer autoloader
-\Asgard\Core\App::loadDefaultApp();
+
+function randstr($length=10, $validCharacters = 'abcdefghijklmnopqrstuxyvwzABCDEFGHIJKLMNOPQRSTUXYVWZ0123456789') {
+	$validCharNumber = strlen($validCharacters);
+
+	$result = '';
+
+	for ($i = 0; $i < $length; $i++) {
+		$index = mt_rand(0, $validCharNumber - 1);
+		$result .= $validCharacters[$index];
+	}
+
+	return $result;
+}
 
 if(file_exists('config/config.php'))
 	echo 'File "config/config.php" already exists.'."\n";
 else {
 	$config = file_get_contents(__DIR__.'/config.php.sample');
-	$key = \Asgard\Utils\Tools::randStr(10);
+	$key = randStr(10);
 	$config = str_replace('_KEY_', $key, $config);
 
-	if(\Asgard\Utils\FileManager::put('config/config.php', $config) !== false)
+	mkdir(_DIR_.'config');
+	if(file_put_contents(_DIR_.'config/config.php', $config) !== false)
 		echo 'Configuration created with success.'."\n";
 	else
 		echo 'Configuration creation failed.'."\n";			
