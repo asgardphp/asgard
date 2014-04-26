b2d474bb465010289d68964eabc65c584846f898
Michel Hognerud <leyou.m@gmail.com>
2014-04-27 02:12:32 +0800

    fixes

--- Node.php
+++ Node.php
@@ -1,17 +1,32 @@
 <?php
 namespace Asgard\Xpath;
 
+/**
+ * Xpath handler for a node.
+ * 
+ * @author Michel Hognerud <michel@hognerud.net>
+*/
 class Node {
 	protected $domnode;
 	protected $xpath;
-
+	
+	/**
+	 * Constructor
+	 * 
+	 * @param \DOMNode domnode
+	*/
 	public function __construct($domnode) {
 		if(!$domnode)
 			return;
 		$this->domnode = $domnode;
 		$this->xpath = static::nodeToXpath($domnode);
 	}
-
+	
+	/**
+	 * Returns the previous node.
+	 * 
+	 * @return \Asgard\Xpath\Node
+	*/
 	public function prev() {
 		$prev = $this->domnode;
 		while(true) {
@@ -22,7 +37,12 @@ class Node {
 				return new Node($prev);
 		}
 	}
-
+	
+	/**
+	 * Returns the next node.
+	 * 
+	 * @return \Asgard\Xpath\Node
+	*/
 	public function next() {
 		$next = $this->domnode;
 		while(true) {
@@ -33,56 +53,45 @@ class Node {
 				return new Node($next);
 		}
 	}
-
+	
 	/**
-	* Convert a DOMNode object to a DOMXPath object
-	*/
-	protected static function nodeToXpath($domnode) {
-		$dom = new \DOMDocument();
-		$dom->formatOutput = true;
-		$node = $dom->importNode($domnode, true);
-		$dom->appendChild($node);
-		return new \DOMXPath($dom);
-	}
-
-	/**
-	* Return the variable xpath (DOMXPath)
+	 * Returns the variable xpath (DOMXPath).
+	 * 
+	 * @return \DOMXPath
 	*/
 	public function getXpath() {
 		return $this->xpath;
 	}
-
+	
 	/**
-	* Return the variable domnode (DOMNode)
+	 * Returns the variable domnode (DOMNode).
+	 * 
+	 * @return \DOMNode
 	*/
 	public function getNode() {
 		return $this->domnode;
 	}
-
+	
 	/**
-	* Return an attribute of the node
+	 * Returns an attribute of the node.
+	 * 
+	 * @param string attr The attribute name.
+	 * 
+	 * @return string
 	*/
 	public function getAttribute($attr) {
 		if($this->domnode === null)
 			return null;
 		return $this->domnode->getAttribute($attr);
 	}
-
+	
 	/**
-	* Return the inner html of a DOMNode object
-	*/
-	protected static function getInnerHTML($node) {
-		if(!$node)
-			return null;
-		$innerHTML= ''; 
-		$children = $node->childNodes; 
-		foreach ($children as $child)
-			$innerHTML .= $child->ownerDocument->saveHTML($child);
-		return $innerHTML; 
-	}
-
-	/**
-	* Return the html for a specific children
+	 * Returns the html for a specific children.
+	 * 
+	 * @param string path Path to the node.
+	 * @param integer pos Element position.
+	 * 
+	 * @return string The html code of the node.
 	*/
 	public function html($path=null, $pos=0) {
 		if($this->xpath === null)
@@ -96,22 +105,14 @@ class Node {
 			return $item->html();
 		}
 	}
-
+	
 	/**
-	* Return the inner xml of a DOMNode object
-	*/
-	protected static function getInnerXML($node) {
-		if($this->xpath === null)
-			return null;
-		$innerHTML= ''; 
-		$children = $node->childNodes; 
-		foreach ($children as $child)
-			$innerHTML .= $child->ownerDocument->saveXML($child);
-		return $innerHTML; 
-	}
-
-	/**
-	* Return the inner xml for a specific children
+	 * Returns the inner xml for a specific children.
+	 * 
+	 * @param string path Path to the node.
+	 * @param integer pos Element position.
+	 * 
+	 * @return string The html code of the node.
 	*/
 	public function xml($path=null, $pos=0) {
 		if($this->xpath === null)
@@ -125,9 +126,14 @@ class Node {
 			return $item->xml();
 		}
 	}
-
+	
 	/**
-	* Return the text for a specific children
+	 * Returns the text for a specific children.
+	 * 
+	 * @param string path Path to the node.
+	 * @param integer pos Element position.
+	 * 
+	 * @return string The text of the node.
 	*/
 	public function text($path=null, $pos=0) {
 		if($this->xpath === null)
@@ -141,18 +147,27 @@ class Node {
 			return $item->text();
 		}
 	}
-
+	
 	/**
-	* Return a specific children
+	 * Returns the first Node object from xpath.
+	 * 
+	 * @param string path Path to the node.
+	 * @param integer pos Element position.
+	 * 
+	 * @return \Asgard\Xpath\Node
 	*/
 	public function item($path, $pos=0) {
 		if($this->xpath === null)
 			return new Node(null);
 		return new Node($this->xpath->evaluate($path)->item($pos));
 	}
-
+	
 	/**
-	* Return an array of children
+	 * Returns an array of children.
+	 * 
+	 * @param string path Path to the nodes.
+	 * 
+	 * @return array Array of \Asgard\Xpath\Node objects.
 	*/
 	public function items($path) {
 		if($this->xpath === null)
@@ -162,5 +177,54 @@ class Node {
 		foreach($nodes as $n)
 			$r[] = new Node($n);
 		return $r;
+	}
+	
+	/**
+	 * Converts a DOMNode object to a DOMXPath object.
+	 * 
+	 * @param \DOMNode domnode
+	 * 
+	 * @return \DOMXPath
+	*/
+	protected static function nodeToXpath($domnode) {
+		$dom = new \DOMDocument();
+		$dom->formatOutput = true;
+		$node = $dom->importNode($domnode, true);
+		$dom->appendChild($node);
+		return new \DOMXPath($dom);
+	}
+	
+	/**
+	 * Returns the inner html of a DOMNode object.
+	 * 
+	 * @param \DOMNode node
+	 * 
+	 * @return string HTML code
+	*/
+	protected static function getInnerHTML($node) {
+		if(!$node)
+			return null;
+		$innerHTML= ''; 
+		$children = $node->childNodes; 
+		foreach ($children as $child)
+			$innerHTML .= $child->ownerDocument->saveHTML($child);
+		return $innerHTML; 
+	}
+	
+	/**
+	 * Returns the inner xml of a DOMNode object.
+	 * 
+	 * @param \DOMNode node
+	 * 
+	 * @return string XML code
+	*/
+	protected static function getInnerXML($node) {
+		if($this->xpath === null)
+			return null;
+		$innerHTML= ''; 
+		$children = $node->childNodes; 
+		foreach ($children as $child)
+			$innerHTML .= $child->ownerDocument->saveXML($child);
+		return $innerHTML; 
 	}
 }
