a86dd6aee26e6b1f3231d8904248a49ca806b43b
Michel Hognerud <leyou.m@gmail.com>
2014-04-28 11:30:07 +0800

    bundle

---
+++ Bundle.php
@@ -0,0 +1,12 @@
+<?php
+namespace Asgard\Data;
+
+class Bundle extends \Asgard\Core\BundleLoader {
+	public function load($queue) {
+		parent::load($queue);
+	}
+
+	public function run() {
+		parent::run();
+	}
+}
