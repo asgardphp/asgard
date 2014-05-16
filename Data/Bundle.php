ad4658f99c87176d98dbe18b846e66b602f8fedd
Michel Hognerud <leyou.m@gmail.com>
2014-05-16 22:00:24 +0930

    removing data from framework

--- Data/Bundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Asgard\Data;
-
-class Bundle extends \Asgard\Core\BundleLoader {
-	public function load(\Asgard\Core\BundlesManager $queue) {
-		\Asgard\Core\App::instance()->register('data', function() { return new Data(\Asgard\Core\App::get('db')); } );
-		parent::load($queue);
-	}
-}
--- Data/asgard-install.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once 'paths.php';
-require_once _VENDOR_DIR_.'autoload.php'; #composer autoloader
-\Asgard\Core\App::loadDefaultApp();
-
-\Asgard\Orm\MigrationsManager::addMigrationFile(__DIR__.'/migrations/Data.php');
-\Asgard\Orm\MigrationsManager::migrate('Data');
