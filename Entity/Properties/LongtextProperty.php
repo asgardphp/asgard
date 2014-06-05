8483d0a14711ce8d8aa6d333b2b4ba20d7691ab9
Michel Hognerud <leyou.m@gmail.com>
2014-06-05 16:00:27 +0930

    renaming LongTextProperty.php

--- Entity/Properties/LongtextProperty.php
+++ Entity/Properties/LongtextProperty.php
@@ -1,7 +1,7 @@
 <?php
 namespace Asgard\Entity\Properties;
 
-class LongTextProperty extends \Asgard\Entity\Property {
+class LongtextProperty extends \Asgard\Entity\Property {
 	public function getSQLType() {
 		return 'text';
 	}
