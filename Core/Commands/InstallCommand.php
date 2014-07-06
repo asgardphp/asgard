da10047bd046b2e502fdb0400234e13cf1208abb
Michel Hognerud <leyou.m@gmail.com>
2014-07-06 20:12:31 +0800

    fixing Publisher namespace

--- Core/Commands/InstallCommand.php
+++ Core/Commands/InstallCommand.php
@@ -5,6 +5,7 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputArgument;
+use Asgard\Core\Publisher;
 
 class InstallCommand extends \Asgard\Console\Command {
 	protected $name = 'install';
