bef547a18168bed69faf93d05122d01bdc17907c
Michel Hognerud <leyou.m@gmail.com>
2014-07-11 13:04:12 +0200

    fixing annotations

--- Hook/Tests/Fixtures/Hooks.php
+++ Hook/Tests/Fixtures/Hooks.php
@@ -3,7 +3,7 @@ namespace Asgard\Hook\Tests\Fixtures;
 
 class Hooks extends \Asgard\Hook\HooksContainer {
 	/**
-	@Hook("test")
+	 * @Hook("test")
 	*/
 	public static function foo() {
 		return 'bar';
--- Http/Tests/Fixtures/Controllers/FooController.php
+++ Http/Tests/Fixtures/Controllers/FooController.php
@@ -3,12 +3,12 @@ namespace Asgard\Http\Tests\Fixtures\Controllers;
 
 class FooController extends \Asgard\Http\Controller {
 	/**
-	@Route(host = "example.com", method = "get", name = "foo", value = "page/:id", requirements = {
-		"src" = {
-			"type" = "regex",
-			"regex" = ".+"
-		}	
-	})
+	 * @Route(host = "example.com", method = "get", name = "foo", value = "page/:id", requirements = {
+	 * 	"src" : {
+	 * 		"type" : "regex",
+	 * 		"regex" : ".+"
+	 * 	}	
+	* })
 	*/
 	public function pageAction(\Asgard\Http\Request $request) {
 		return 'hello!';
