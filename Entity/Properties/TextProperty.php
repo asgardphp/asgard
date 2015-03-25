d6024667738ce4b138ab24ab1dda97dcc27a70fe
Michel <leyou.m@gmail.com>
2015-03-25 15:27:39 +0100

    fix default text length

--- Entity/Properties/TextProperty.php
+++ Entity/Properties/TextProperty.php
@@ -9,10 +9,19 @@ class TextProperty extends \Asgard\Entity\Property {
 	/**
 	 * {@inheritDoc}
 	 */
+	public function __construct(array $params) {
+		if(!isset($params['length']))
+			$params['length'] = 65535;
+		parent::__construct($params);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
 	public function getORMParameters() {
 		return [
 			'type' => 'text',
-			'length' => $this->get('length') ? $this->get('length'):65535,
+			'length' => $this->get('length'),
 		];
 	}
 }
