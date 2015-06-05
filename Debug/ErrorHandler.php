8207e9ecf1deba9bc8ac52d10edd25e3f4f33882
Michel <leyou.m@gmail.com>
2015-06-05 14:24:59 +0200

    hide errors when registering errorhandler

--- Debug/ErrorHandler.php
+++ Debug/ErrorHandler.php
@@ -56,6 +56,10 @@ class ErrorHandler {
 	 * @return ErrorHandler
 	 */
 	public static function register() {
+		ini_set('display_errors', 0);
+		ini_set('display_startup_errors', 0);
+		error_reporting(-1);
+
 		static::$reservedMemory = str_repeat('a', 10240);
 		static::$errorAtStart = error_get_last();
 
