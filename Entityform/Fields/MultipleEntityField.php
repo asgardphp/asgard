57aa19b090d90b1d1965ed179ac8fe1f77377c54
michel hognerud <leyou.m@gmail.com>
2016-02-29 15:52:04 +0100

    entityform: multipleentityfield

---
+++ Entityform/Fields/MultipleEntityField.php
@@ -0,0 +1,52 @@
+<?php
+namespace Asgard\Entityform\Fields;
+
+/**
+ * File field.
+ * @author Michel Hognerud <michel@hognerud.com>
+ */
+class MultipleEntityField extends \Asgard\Form\Field {
+	/**
+	 * {@inheritDoc}
+	 */
+	protected $widget = 'multipleselect';
+	/**
+	 * ORM.
+	 * @var \Asgard\Orm\ORMInterface
+	 */
+	protected $orm;
+	/**
+	 * Array of entities.
+	 * @var array
+	 */
+	protected $entities;
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public function __construct(array $options=[]) {
+		parent::__construct($options);
+
+		if(!isset($this->options['orm']))
+			throw new \Exception('Option "orm" must be given.');
+		$orm = $this->orm = $this->options['orm'];
+
+		$this->options['choices'] = [];
+		foreach($orm as $e)
+			$this->options['choices'][$e->id] = (string)$e;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public function value() {
+		$ids = $this->value;
+		if($ids !== null && $this->entities === null) {
+			$this->entities = [];
+			foreach($ids as $id)
+				$this->entities[] = $this->orm->load($id);
+		}
+
+		return $this->entities;
+	}
+}
--- Entityform/Tests/EntityFieldTest.php
+++ Entityform/Tests/EntityFieldTest.php
@@ -36,4 +36,44 @@ class EntityFieldTest extends \PHPUnit_Framework_TestCase {
 		$this->assertEquals('<select name="field" id="field"><option value="1" selected="selected">bob</option></select>', (string)($form['field']->def()));
 		$this->assertInstanceOf('Asgard\Entityform\Tests\Entities\User', $form['field']->value());
 	}
+
+	public function testMultiple() {
+		#Dependencies
+		$db = new \Asgard\Db\DB([
+			'driver' => 'sqlite',
+			'database' => ':memory:',
+		]);
+		$em = new \Asgard\Entity\EntityManager;
+		$dataMapper = new \Asgard\Orm\DataMapper($db, $em);
+
+		#Fixtures
+		$schema = new \Asgard\Db\Schema($db);
+		$schema->drop('user');
+		(new \Asgard\Orm\ORMMigrations($dataMapper))->autoMigrate([
+			$em->get('Asgard\Entityform\Tests\Entities\User'),
+		]);
+
+		$user = $dataMapper->create('Asgard\Entityform\Tests\Entities\User', [
+			'id'  => 1,
+			'name'=>'bob',
+		]);
+		$user = $dataMapper->create('Asgard\Entityform\Tests\Entities\User', [
+			'id'  => 2,
+			'name'=>'bob',
+		]);
+
+		$form = new \Asgard\Form\Form;
+		$request = new \Asgard\Http\Request;
+		$request->setMethod('post');
+		$request->post['field'] = ['1', '2'];
+		$form->setRequest($request);
+
+		$form['field'] = new \Asgard\Entityform\Fields\MultipleEntityField([
+			'orm' => $dataMapper->orm('Asgard\Entityform\Tests\Entities\User'),
+		]);
+
+		$this->assertEquals('<select name="field[]" id="field" multiple="multiple"><option value="2" selected="selected">bob</option><option value="1" selected="selected">bob</option></select>', (string)($form['field']->def()));
+		$this->assertInstanceOf('Asgard\Entityform\Tests\Entities\User', $form['field']->value()[0]);
+		$this->assertInstanceOf('Asgard\Entityform\Tests\Entities\User', $form['field']->value()[1]);
+	}
 }
