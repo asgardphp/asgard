df8932b0e509562f50634c609071589b9e2ddcd2
michel hognerud <leyou.m@gmail.com>
2016-03-03 16:12:58 +0100

    migration: fix

--- Migration/MigrationManager.php
+++ Migration/MigrationManager.php
@@ -71,10 +71,6 @@ class MigrationManager implements MigrationManagerInterface {
 		$down = implode("\n\t\t", explode("\n", $down));
 		$name = ucfirst(strtolower($name));
 
-		$dst = $this->directory.'/'.$name.'_'.time().'.php';
-		$dst = \Asgard\File\FileSystem::getNewFilename($dst);
-		$name = str_replace('.php', '', basename($dst));
-
 		$migration = '<?php
 class '.$name.' extends '.$class.' {
 	public function up() {
